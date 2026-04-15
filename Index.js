900b4e0c100f220c703dfaf23f068abf5e080af0
darcioliveiradeveloper <darcioliveiradeveloper@gmail.com>
2026-04-14 21:51:30 -0300

    criando menu

--- Index.js
+++ Index.js
@@ -1,1 +1,6 @@
-// vamos fazer aqui um sistema de lembretes, onde o usuário pode criar, editar e excluir lembretes.
+// vamos fazer aqui um sistema de lembretes, 
+// onde o usuário pode criar, editar e excluir lembretes,
+// e os lembretes serão armazenados em um array de objetos.    
+
+const readline = require('readline')
+
